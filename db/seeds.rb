2400c8cbd05dfe81213de5a35b3424790d9e523f
Lautaro <lautarocastillo.93@gmail.com>
2017-02-21 11:57:21 -0300

    actualized seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Category.destroy_all
+
+3.times do |i|
+	a=Category.create(name: "categoria #{i}")
+	a.products.build(name: "producto #{i}", cost: i).save
+	Product.create!(name: "super product #{i}", category_id:a.id, cost: i*2)
+	Product.last.delete
+end
